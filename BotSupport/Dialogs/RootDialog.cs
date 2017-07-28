2f53d217aeaedc6339dd1bd3f7989ccffe530e06
Алексей Токарев <tokalig@mail.ru>
2017-07-28 19:54:43 +0300

    Доработать коректность вывода информации на запросы. Идет самопроизвольное обращение к методам QnA Maker. Где-то произошла осечка с проверкой условий для последующе

--- BotSupport/Dialogs/RootDialog.cs
+++ BotSupport/Dialogs/RootDialog.cs
@@ -13,6 +13,9 @@ namespace BotSupport.Dialogs
         private string platform; //Площадка, по которой пользователь хочет получить консультацию ("223-ФЗ", "44-ФЗ", "615-ФЗ", "Имущество", "РТС-Маркет")
         private string role; // Какова роль пользователя ("Заказчик", "Поставщик")
         private string type; // Кем является пользователь ("ИП", "ФЛ", "ЮЛ")
+        private bool parametrs = false;
+        private string knowledgebaseId; // Идентификатор базы знаний для бота QnA Maker
+        private string qnamakerSubscriptionKey; // Использование ключа подписи в QnA Maker
 
         public Task StartAsync(IDialogContext context)
         {
@@ -24,51 +27,192 @@ namespace BotSupport.Dialogs
         private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
         {
             var activity = await result as Activity;
-
-            //// calculate something for us to return
-            //int length = (activity.Text ?? string.Empty).Length;
-            if (string.IsNullOrEmpty(platform) || string.IsNullOrEmpty(role) || string.IsNullOrEmpty(type))
+            if (parametrs == false)
             {
-                await context.PostAsync("Необходимая информация еще не собрана!");
-
-                if (!string.IsNullOrWhiteSpace(activity.Text))
+                //// calculate something for us to return
+                //int length = (activity.Text ?? string.Empty).Length;
+                if (string.IsNullOrEmpty(platform) || string.IsNullOrEmpty(role) || string.IsNullOrEmpty(type))
                 {
-                    var apiAiResponse = ApiAiRequest.ApiAiBotRequest(activity.Text);
-                    
-                    // Если есть ошибки
-                    if (apiAiResponse.Errors != null && apiAiResponse.Errors.Count > 0)
+
+
+                    if (!string.IsNullOrWhiteSpace(activity.Text))
                     {
-                        await context.PostAsync("Что-то пошло не так, повторите попытку");
-                    } 
-                    
-                    // Если нет ошибок
-                    else 
-                    {
-                        if (!string.IsNullOrEmpty(platform))
+                        var apiAiResponse = ApiAiRequest.ApiAiBotRequest(activity.Text);
+
+                        // Если есть ошибки
+                        if (apiAiResponse.Errors != null && apiAiResponse.Errors.Count > 0)
                         {
-                            if (platform != apiAiResponse.Platform.ToString())
-                                platform = apiAiResponse.Platform;
+                            await context.PostAsync("Что-то пошло не так, повторите попытку");
                         }
+
+                        // Если нет ошибок
                         else
                         {
-                            platform = apiAiResponse.Platform;
+                            // Проверка наличия, добавление или редактирование параметра "Площадка"
+                            if (!string.IsNullOrEmpty(platform))
+                            {
+                                if ((platform != apiAiResponse.Platform) &&
+                                    (!string.IsNullOrEmpty(apiAiResponse.Platform)))
+                                    platform = apiAiResponse.Platform;
+                            }
+                            else
+                            {
+                                platform = apiAiResponse.Platform;
+                            }
+
+                            // Проверка наличия, добавление или редактирование параметра "Роль"
+                            if (!string.IsNullOrEmpty(role))
+                            {
+                                if ((role != apiAiResponse.Role) && (!string.IsNullOrEmpty(apiAiResponse.Role)))
+                                    role = apiAiResponse.Role;
+                            }
+                            else
+                            {
+                                role = apiAiResponse.Role;
+                            }
+
+                            // Проверка наличия, добавление или редактирование параметра "Тип"
+                            if (!string.IsNullOrEmpty(type))
+                            {
+                                if ((type != apiAiResponse.Type) && (!string.IsNullOrEmpty(apiAiResponse.Type)))
+                                    type = apiAiResponse.Type;
+                            }
+                            else
+                            {
+                                type = apiAiResponse.Type;
+                            }
                         }
-
-                        if (string.IsNullOrEmpty(role)) role = apiAiResponse.Role;
-                        if (string.IsNullOrEmpty(type)) type = apiAiResponse.Type;
-
-
                     }
+
+                    // return our reply to the user
+                    //await context.PostAsync($"Текст твоего запроса => {activity.Text}");
+                    else
+                    {
+                        await context.PostAsync("Что-то пошло не так, повторите попытку");
+                    }
+
+                    // Идет проверка наличия всех заполненных и незаполненных параметров с последующим информированием пользователя
+                    if (string.IsNullOrEmpty(platform) || string.IsNullOrEmpty(role) || string.IsNullOrEmpty(type))
+                    {
+                        await context.PostAsync(CheckParametrs());
+                    }
+                    else
+                    {
+                        parametrs = true;
+                        await context.PostAsync("Напишите теперь интересующую Вас тему.");
+                        //await context.PostAsync(QnABotResponse(activity.Text));
+                    }
+
                 }
-                // return our reply to the user
-                //await context.PostAsync($"Текст твоего запроса => {activity.Text}");
                 else
                 {
-                    await context.PostAsync("Что-то пошло не так, повторите попытку");
+                    parametrs = true;
+                    await context.PostAsync("Напишите теперь интересующую Вас тему.");
                 }
             }
-            else await context.PostAsync($"Площадка: {platform} Роль: {role} Тип: {type}");
+
+            if (!string.IsNullOrEmpty(activity.Text))
+            {
+                await context.PostAsync(QnABotResponse(activity.Text));
+            }
+
+            else
+            {
+                await context.PostAsync("Извините, что-то пошло не так. Повторите, пожалуйста попытку");
+            }
+
             context.Wait(MessageReceivedAsync);
+        }
+
+        /// <summary>
+        /// Определяет, какие параметры у нас заполнены, а какие нет
+        /// </summary>
+        /// <returns></returns>
+        string CheckParametrs()
+        {
+            // Не заполнены поля "Площадка", "Роль", "Тип"
+            if (string.IsNullOrEmpty(platform) && string.IsNullOrEmpty(role) && string.IsNullOrEmpty(type))
+            {
+                return "Прежде чем дать консультацию, я должен понимать, кем Вы являетесь. Дайте мне, пожалуйста, некоторую информацию о Вас (отвечайте на вопросы по очереди):\n" +
+                    "1. Кто Вы: заказчик или поставщик?\n" +
+                    "2. Какая площадка Вас интересует: 223-ФЗ, 44-ФЗ, 615-ФЗ, Имущество или РТС-Маркет?\n" +
+                    "3. К какому типу Вы относитесь: индивидуальный предприниматель, физическое лицо или юридическое лицо?\n";
+            }
+
+            // Заполнено: "Площадка"
+            // Не заполнено: "Роль" и "Тип"
+            if (!string.IsNullOrEmpty(platform) && string.IsNullOrEmpty(role) && string.IsNullOrEmpty(type))
+            {
+                return "Для более полной информации я еще должен уточнить некоторые детали:\n" +
+                       "1. Кто Вы: заказчик или поставщик?\n" +
+                       "2. К какому типу Вы относитесь: индивидуальный предприниматель, физическое лицо или юридическое лицо?\n";
+            }
+
+            // Заполнено:  "Роль"
+            // Не заполнено: "Площадка" и "Тип"
+            if (string.IsNullOrEmpty(platform) && !string.IsNullOrEmpty(role) && string.IsNullOrEmpty(type))
+            {
+                return "Для более полной информации я еще должен уточнить некоторые детали:\n" +
+                       "1. Какая площадка Вас интересует: 223-ФЗ, 44-ФЗ, 615-ФЗ, Имущество или РТС-Маркет?\n" +
+                       "2. К какому типу Вы относитесь: индивидуальный предприниматель, физическое лицо или юридическое лицо?\n";
+            }
+
+            // Заполнено: "Тип"
+            // Не заполнено: "Площадка" и "Роль"
+            if (string.IsNullOrEmpty(platform) && string.IsNullOrEmpty(role) && !string.IsNullOrEmpty(type))
+            {
+                return "Для более полной информации я еще должен уточнить некоторые детали:\n" +
+                       "1. Кто Вы: заказчик или поставщик?\n" +
+                       "2. Какая площадка Вас интересует: 223-ФЗ, 44-ФЗ, 615-ФЗ, Имущество или РТС-Маркет?\n";
+            }
+
+            // Заполнено: "Площадка" и "Роль"
+            // Не заполнено: "Тип"
+            if (!string.IsNullOrEmpty(platform) && !string.IsNullOrEmpty(role) && string.IsNullOrEmpty(type))
+            {
+                return
+                    "Для более полной информации я еще должен уточнить к какому типу Вы относитесь: " +
+                    "индивидуальный предприниматель, физическое лицо или юридическое лицо?\n";
+            }
+
+            // Заполнено: "Площадка" и "Тип"
+            // Не заполнено: "Роль" 
+            if (!string.IsNullOrEmpty(platform) && string.IsNullOrEmpty(role) && !string.IsNullOrEmpty(type))
+            {
+                return "Для более полной информации я еще должен уточнить кто Вы: заказчик или поставщик?\n";
+            }
+
+            // Заполнено: "Роль" и "Тип"
+            // Не заполнено: "Площадка"
+            if (string.IsNullOrEmpty(platform) && !string.IsNullOrEmpty(role) && !string.IsNullOrEmpty(type))
+            {
+                return
+                    "Для более полной информации я еще должен уточнить какая площадка Вас интересует: " +
+                    "223-ФЗ, 44-ФЗ, 615-ФЗ, Имущество или РТС-Маркет?\n";
+            }
+
+            return "Что-то пошло не так, попробуйте еще раз";
+        }
+
+        public void QnAMakerKey()
+        {
+            if (platform == "223-ФЗ")
+            {
+                knowledgebaseId = "da50c6c1-0e1f-467f-b94a-f82c0b0e1ac7";
+                qnamakerSubscriptionKey = "850a8ac4def146498ab7e2161cd87c9d";
+            }
+
+            if (platform == "44-ФЗ") {};
+            if (platform == "615-ФЗ") {};
+            if (platform == "Имущество") {};
+            if (platform == "РТС-Маркет") {};
+        }
+
+        string QnABotResponse(string qnaResponse)
+        {
+            QnAMakerKey();
+            string qnaResult = QnARequest.QnAResponse(knowledgebaseId, qnamakerSubscriptionKey, qnaResponse);
+            return qnaResult;
         }
     }
 }
--- QnA/QnARequest.cs
+++ QnA/QnARequest.cs
@@ -7,70 +7,57 @@ namespace QnA
 {
     public class QnARequest
     {
-        // Идентификатор базы знаний
-        const string KnowledgebaseId = "da50c6c1-0e1f-467f-b94a-f82c0b0e1ac7";
-
-        // Использование ключа подписи в QnA Maker
-        const string QnamakerSubscriptionKey = "850a8ac4def146498ab7e2161cd87c9d";
-
         // Адрес для запроса к базе знаний QnA Maker
         private const string UrlAddress = "https://westus.api.cognitive.microsoft.com/qnamaker/v2.0";
 
-        static void Main(string[] args)
+        public static string QnAResponse(string knowledgebaseId, string qnamakerSubscriptionKey, string query)
         {
-            Console.Title = "Console-Bot";
-            for (;;)
+            string responseString;
+            string qnaResult;
+            // Вписать адрес для работы с классом URI
+            var qnamakerUriBase = new Uri(UrlAddress);
+            var builder = new UriBuilder($"{qnamakerUriBase}/knowledgebases/{knowledgebaseId}/generateAnswer");
+
+            // Добавление вопроса как части тела
+            var postBody = $"{{\"question\": \"{query}\"}}";
+
+            // Отсылаем ПОСТ запрос
+            using (var client = new WebClient())
             {
-                Console.Clear();
-                string responseString;
-                Console.Write("Введите запрос: ");
+                // Изменияем кодировку
+                client.Encoding = System.Text.Encoding.UTF8;
 
-                // Запрос пользователя
-                var query = Console.ReadLine();
+                // Добавляем заголовок ключа подписи
+                client.Headers.Add("Ocp-Apim-Subscription-Key", qnamakerSubscriptionKey);
+                client.Headers.Add("Content-Type", "application/json");
 
-                // Вписать адрес для работы с классом URI
-                var qnamakerUriBase = new Uri(UrlAddress);
-                var builder = new UriBuilder($"{qnamakerUriBase}/knowledgebases/{KnowledgebaseId}/generateAnswer");
-
-                // Добавление вопроса как части тела
-                var postBody = $"{{\"question\": \"{query}\"}}";
-
-                // Отсылаем ПОСТ запрос
-                using (var client = new WebClient())
+                try
                 {
-                    // Изменияем кодировку
-                    client.Encoding = System.Text.Encoding.UTF8;
-
-                    // Добавляем заголовок ключа подписи
-                    client.Headers.Add("Ocp-Apim-Subscription-Key", QnamakerSubscriptionKey);
-                    client.Headers.Add("Content-Type", "application/json");
-
-                    try
-                    {
-                        responseString = client.UploadString(builder.Uri, postBody);
-                    }
-                    catch
-                    {
-                        continue;
-                    }
-
-                    // Создам переменную, которая из класса ResponseModel возвращает нам PossibleAnswer
-                    var response = JsonConvert.DeserializeObject<Response>(responseString);
-
-                    var firstOrDefault = response.Answers.FirstOrDefault();
-
-                    if (firstOrDefault != null)
-                    {
-                        Console.WriteLine(firstOrDefault.PossibleAnswer);
-                    }
-                    else
-                    {
-                        continue;
-                    }
+                    responseString = client.UploadString(builder.Uri, postBody);
+                }
+                catch
+                {
+                    return "Извините, произошла ошибка, попробуйте еще раз";
                 }
 
-                Console.ReadLine();
+                // Создам переменную, которая из класса ResponseModel возвращает нам PossibleAnswer
+                var response = JsonConvert.DeserializeObject<Response>(responseString);
+
+                var firstOrDefault = response.Answers.FirstOrDefault();
+                qnaResult = firstOrDefault.PossibleAnswer.ToString();
+
+                //if (firstOrDefault != null)
+                //{
+                //    return qnaResult;
+                //}
+                //else
+                //{
+                //    return "Извините, произошла ошибка, попробуйте еще раз";
+                //}
             }
+
+            if (!string.IsNullOrEmpty(qnaResult)) return qnaResult;
+            else return "Извините, произошла ошибка, попробуйте еще раз";
         }
     }
 }
