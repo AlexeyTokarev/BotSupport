57aa89b8bbfcee834bdb83922a17f13d03764d5d
Aleksey <tokalig@mail.ru>
2017-09-05 13:55:46 +0300

    fix1

--- GoogleTablesWorking/AddQuestionInGoogleSheet.cs
+++ GoogleTablesWorking/AddQuestionInGoogleSheet.cs
@@ -27,108 +27,108 @@ namespace GoogleTablesWorking
 
             String serviceAccountEmail = "tessheet3@curious-domain-178413.iam.gserviceaccount.com";
 
-            var certificate = new X509Certificate2(KeyDirectory, "notasecret", X509KeyStorageFlags.Exportable);
+        //    var certificate = new X509Certificate2(KeyDirectory, "notasecret", X509KeyStorageFlags.Exportable);
 
-            ServiceAccountCredential credential = new ServiceAccountCredential(
-                new ServiceAccountCredential.Initializer(serviceAccountEmail)
-                {
-                    Scopes = new[] { SheetsService.Scope.Spreadsheets }
-                }.FromCertificate(certificate));
+        //    ServiceAccountCredential credential = new ServiceAccountCredential(
+        //        new ServiceAccountCredential.Initializer(serviceAccountEmail)
+        //        {
+        //            Scopes = new[] { SheetsService.Scope.Spreadsheets }
+        //        }.FromCertificate(certificate));
 
-            // Create the service.
-            var service = new SheetsService(new BaseClientService.Initializer()
-            {
-                HttpClientInitializer = credential,
-                ApplicationName = AppName,
-            });
+        //    // Create the service.
+        //    var service = new SheetsService(new BaseClientService.Initializer()
+        //    {
+        //        HttpClientInitializer = credential,
+        //        ApplicationName = AppName,
+        //    });
 
-            FillSpreadsheet(service, SpreadsheetId, Data);
-        }
+        //    FillSpreadsheet(service, SpreadsheetId, Data);
+        //}
 
-        /// <summary>
-        /// Метод реализует заполнение таблицы данными
-        /// </summary>
-        /// <param name="service"></param>
-        /// <param name="spreadsheetId"></param>
-        /// <param name="data"></param>
-        private static void FillSpreadsheet(SheetsService service, string spreadsheetId, string[] data)
-        {
-            List<Request> requests = new List<Request>();
-            List<CellData> values = new List<CellData>();
-
-
-            foreach (var a in data)
-            {
-                values.Add(new CellData
-                {
-                    UserEnteredValue = new ExtendedValue { StringValue = a }
-                });
-            }
-
-            requests.Add(new Request
-            {
-                UpdateCells = new UpdateCellsRequest
-                {
-                    Start = new GridCoordinate
-                    {
-                        SheetId = 0,
-                        RowIndex = FindFreeRow(service, spreadsheetId),
-                        ColumnIndex = 0
-                    },
-                    Rows = new List<RowData>
-                    {
-                        new RowData { Values = values }
-                    },
-                    Fields = "userEnteredValue"
-                }
-
-            });
+        ///// <summary>
+        ///// Метод реализует заполнение таблицы данными
+        ///// </summary>
+        ///// <param name="service"></param>
+        ///// <param name="spreadsheetId"></param>
+        ///// <param name="data"></param>
+        //private static void FillSpreadsheet(SheetsService service, string spreadsheetId, string[] data)
+        //{
+        //    List<Request> requests = new List<Request>();
+        //    List<CellData> values = new List<CellData>();
 
 
-            BatchUpdateSpreadsheetRequest busr = new BatchUpdateSpreadsheetRequest
-            {
-                Requests = requests
-            };
-            service.Spreadsheets.BatchUpdate(busr, spreadsheetId).Execute();
-        }
+        //    foreach (var a in data)
+        //    {
+        //        values.Add(new CellData
+        //        {
+        //            UserEnteredValue = new ExtendedValue { StringValue = a }
+        //        });
+        //    }
 
-        /// <summary>
-        /// Метод реализует проверку на наличие пустой строки для удобства записи
-        /// </summary>
-        /// <param name="service"></param>
-        /// <param name="spreadsheetId"></param>
-        /// <returns></returns>
-        private static int FindFreeRow(SheetsService service, string spreadsheetId)
-        {
-            SpreadsheetsResource.ValuesResource.GetRequest.ValueRenderOptionEnum valueRenderOption = 0;
-            SpreadsheetsResource.ValuesResource.GetRequest.DateTimeRenderOptionEnum dateTimeRenderOption = 0;
+        //    requests.Add(new Request
+        //    {
+        //        UpdateCells = new UpdateCellsRequest
+        //        {
+        //            Start = new GridCoordinate
+        //            {
+        //                SheetId = 0,
+        //                RowIndex = FindFreeRow(service, spreadsheetId),
+        //                ColumnIndex = 0
+        //            },
+        //            Rows = new List<RowData>
+        //            {
+        //                new RowData { Values = values }
+        //            },
+        //            Fields = "userEnteredValue"
+        //        }
 
-            bool emptyRow = false;
-            int rowNumber = 1;
+        //    });
 
-            while (emptyRow == false)
-            {
-                string range = $"A{rowNumber}:D{rowNumber}";
 
-                SpreadsheetsResource.ValuesResource.GetRequest request =
-                    service.Spreadsheets.Values.Get(spreadsheetId, range);
-                request.ValueRenderOption = valueRenderOption;
-                request.DateTimeRenderOption = dateTimeRenderOption;
+        //    BatchUpdateSpreadsheetRequest busr = new BatchUpdateSpreadsheetRequest
+        //    {
+        //        Requests = requests
+        //    };
+        //    service.Spreadsheets.BatchUpdate(busr, spreadsheetId).Execute();
+        //}
 
-                ValueRange response = request.Execute();
-                var jsonobj = JsonConvert.SerializeObject(response);
-                dynamic obj = JsonConvert.DeserializeObject(jsonobj);
-                var values = obj.values;
+        ///// <summary>
+        ///// Метод реализует проверку на наличие пустой строки для удобства записи
+        ///// </summary>
+        ///// <param name="service"></param>
+        ///// <param name="spreadsheetId"></param>
+        ///// <returns></returns>
+        //private static int FindFreeRow(SheetsService service, string spreadsheetId)
+        //{
+        //    SpreadsheetsResource.ValuesResource.GetRequest.ValueRenderOptionEnum valueRenderOption = 0;
+        //    SpreadsheetsResource.ValuesResource.GetRequest.DateTimeRenderOptionEnum dateTimeRenderOption = 0;
 
-                if (values != null)
-                {
-                    rowNumber++;
-                    continue;
-                }
+        //    bool emptyRow = false;
+        //    int rowNumber = 1;
 
-                emptyRow = true;
-            }
-            return rowNumber - 1;
+        //    while (emptyRow == false)
+        //    {
+        //        string range = $"A{rowNumber}:D{rowNumber}";
+
+        //        SpreadsheetsResource.ValuesResource.GetRequest request =
+        //            service.Spreadsheets.Values.Get(spreadsheetId, range);
+        //        request.ValueRenderOption = valueRenderOption;
+        //        request.DateTimeRenderOption = dateTimeRenderOption;
+
+        //        ValueRange response = request.Execute();
+        //        var jsonobj = JsonConvert.SerializeObject(response);
+        //        dynamic obj = JsonConvert.DeserializeObject(jsonobj);
+        //        var values = obj.values;
+
+        //        if (values != null)
+        //        {
+        //            rowNumber++;
+        //            continue;
+        //        }
+
+        //        emptyRow = true;
+        //    }
+        //    return rowNumber - 1;
         }
     }
 }
