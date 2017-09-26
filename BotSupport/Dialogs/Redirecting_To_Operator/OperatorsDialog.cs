80cc5334b82ae4fcf2eae476a5e248398b91dbb7
Aleksey <tokalig@mail.ru>
2017-09-26 13:11:46 +0300

    fix5

--- BotSupport/Dialogs/Redirecting_To_Operator/OperatorsDialog.cs
+++ BotSupport/Dialogs/Redirecting_To_Operator/OperatorsDialog.cs
@@ -34,8 +34,8 @@ namespace BotSupport.Dialogs.Redirecting_To_Operator
             string textForOperator = $"Площадка: {platform}\nРоль: {role}\nВопрос: {userQuestion}";
 
             IMessageActivity message = Activity.CreateMessageActivity();
-            message.From = userAccount;
-            message.Recipient = operatorsAccount;
+            message.From = operatorsAccount;
+            message.Recipient = userAccount;
             message.Conversation = new ConversationAccount(id: convId.Id);
             message.Text = textForOperator;
             await connector.Conversations.SendToConversationAsync((Activity)message);
