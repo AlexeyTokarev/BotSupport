1ed25b31db3fdf94bc63fd21b31804cd42e83750
Aleksey <tokalig@mail.ru>
2017-09-26 16:39:38 +0300

    fix14

--- BotSupport/Dialogs/RootDialog.cs
+++ BotSupport/Dialogs/RootDialog.cs
@@ -249,7 +249,7 @@ namespace BotSupport.Dialogs
 
  //---------------- Если оператор присутствует, то пересылать сообщения ему-----------------------------------------------------------------------------------------------------------------------------------------
 
-                    var operatorsAccount = new ChannelAccount("mlh89j6hg7k", "Bot");//("429719242", null); //(OperatorsClass.Id, OperatorsClass.Name);
+                    var operatorsAccount = new ChannelAccount(activity.Recipient.Id, activity.Recipient.Name);//("429719242", null); //(OperatorsClass.Id, OperatorsClass.Name);
                     var userAccount = new ChannelAccount(activity.From.Id, activity.From.Name); //("mlh89j6hg7k", "Bot");
                     var connector = new ConnectorClient(new Uri(activity.ServiceUrl));
 
