5e4fd06e7fb0d46d444a99c16824e9f2e34ae391
Aleksey <tokalig@mail.ru>
2017-09-26 15:34:26 +0300

    fix10

--- BotSupport/Dialogs/Redirecting_To_Operator/OperatorsDialog.cs
+++ BotSupport/Dialogs/Redirecting_To_Operator/OperatorsDialog.cs
@@ -22,7 +22,7 @@ namespace BotSupport.Dialogs.Redirecting_To_Operator
             var activity = await result as Activity;
 
             var operatorsAccount = new ChannelAccount("429719242"); //(OperatorsClass.Id, OperatorsClass.Name);
-            var userAccount = new ChannelAccount(activity.From.Id, activity.From.Name); //("mlh89j6hg7k", "Bot");
+            var userAccount = new ChannelAccount(activity.From.Id); //("mlh89j6hg7k", "Bot");
             var connector = new ConnectorClient(new Uri(activity.ServiceUrl));
 
             if (convId == null)
@@ -42,8 +42,8 @@ namespace BotSupport.Dialogs.Redirecting_To_Operator
             string textForOperator = "Кукусики";//$"Площадка: {platform}\nРоль: {role}\nВопрос: {userQuestion}";
 
             IMessageActivity message = Activity.CreateMessageActivity();
-            message.From = userAccount;
-            message.Recipient = operatorsAccount;
+            message.From = operatorsAccount;
+            message.Recipient = userAccount;
             message.Conversation = new ConversationAccount(id: convId.Id);
             message.Text = textForOperator;
             await connector.Conversations.SendToConversationAsync((Activity)message);
