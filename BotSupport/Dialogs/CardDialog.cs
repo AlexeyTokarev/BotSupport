7e4aeb6432907e9d752e6c3e6fa806b251557507
Aleksey <tokalig@mail.ru>
2017-11-30 22:42:24 +0300

    Попытка вставить табуляцию

--- BotSupport/Dialogs/CardDialog.cs
+++ BotSupport/Dialogs/CardDialog.cs
@@ -75,7 +75,7 @@ namespace BotSupport.Dialogs
                     var heroFB2 = new HeroCard()
                     {
                         Buttons = cardButtonFB2,
-                        Text = null
+                        Text = "\t"
                     };
                     var attachFB1 = heroFB1.ToAttachment();
                     var attachFB2 = heroFB2.ToAttachment();
